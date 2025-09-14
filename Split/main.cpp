a8b1bb30ef30de54317ae6506af155c76234a40e
Илья Щербаков <iliya309@mail.ru>
2025-09-14 12:30:52 +1000

    Split v2 DONE

--- Split/main.cpp
+++ Split/main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 void Print(int arr[], const int  n);
 
-void main() 
+int main() 
 {
 	setlocale(LC_ALL, "");
 	const int n = 10;
-	int odd_nums = 0;
-	int even_nums = 0;
+	int odd_count = 0;
+	int even_count = 0;
 
 	
 	int arr[n];
@@ -17,28 +17,32 @@ void main()
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % 100;
-		if (arr[i] % 2 == 0) even_nums++;
-		else odd_nums++;
+		if (arr[i] % 2 == 0) even_count++;
+		else odd_count++;
 	}
 	
 	Print(arr, n);
 
-	int* even = new int[even_nums];
-	int* odd = new int[odd_nums];
+	int* even = new int[even_count];
+	int* odd = new int[odd_count];
+
+	even_count = odd_count = 0;
 
 	for (int i = 0; i < n; i++)
 	{
-		if (arr[i] % 2 == 0) *(even++) = arr[i];
-		else *(odd++) = arr[i];
+		if (arr[i] % 2 == 0) even[even_count++] = arr[i];
+		else odd[odd_count++] = arr[i];
 	}
 
-	even -= even_nums;
-	odd -= odd_nums;
+	cout << "Четные числа в массиве: ";
+	Print(even, even_count);
+	cout << "Нечетные числа в массиве: ";
+	Print(odd, odd_count);
 
-	Print(even, even_nums);
-	Print(odd, odd_nums);
 	delete[] even;
 	delete[] odd;
+
+	return 0;
 }
 
 
