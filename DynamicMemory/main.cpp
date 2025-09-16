c5749309e625e44cf64f62f6fc6684d82c40b815
Илья Щербаков <iliya309@mail.ru>
2025-09-17 00:34:17 +1000

    push_front, insert, erase DONE

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -6,6 +6,9 @@ using namespace std;
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
 int* push_back(int arr[], int& n); //добавляет значение в конец массива
+int* push_front(int arr[], int& n);
+int* insert(int arr[], int& n);
+int* erase(int arr[], int& n);
 
 void main()
 {
@@ -21,8 +24,17 @@ void main()
 	
 	arr = push_back(arr, n);
 	Print(arr, n);
+
+	arr = push_front(arr, n);
+	Print(arr, n);
+
+	arr = insert(arr, n);
+	Print(arr, n);
+
+	arr = erase(arr, n);
+	Print(arr, n);
+
 	delete[] arr;
-	
 }
 void FillRand(int arr[], const int n)
 {
@@ -57,5 +69,63 @@ int* push_back(int arr[], int& n)
 	arr = buffer;
 
 	n++;
+	return arr;
+}
+
+int* push_front(int arr[], int& n)
+{
+	int value;
+	cout << "Введите добавляемое значение: "; cin >> value;
+	int* buffer = new int[n + 1];
+
+	for (int i = n; i > 0 ; i--)
+	{
+		buffer[i] = arr[i-1];
+	}
+	buffer[0] = value;
+
+	delete[] arr;
+	arr = buffer;
+
+	n++;
+	return arr;
+}
+int* insert(int arr[], int& n)
+{
+	int value, index;
+	cout << "Введите добавляемое значение: "; cin >> value;
+	cout << "Введите индекс для добавляемого значения: "; cin >> index;
+
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		if (i < index) buffer[i] = arr[i];
+		else buffer[i + 1] = arr[i];
+	}
+	buffer[index] = value;
+
+	delete[] arr;
+	arr = buffer;
+
+	n++;
+	return arr;
+}
+int* erase(int arr[], int& n)
+{
+	int index;
+	cout << "Введите индекс для удаляемого значения: "; cin >> index;
+
+	int* buffer = new int[n - 1];
+
+	for (int i = 0; i < n-1; i++)
+	{
+		if (i < index) buffer[i] = arr[i];
+		else buffer[i] = arr[i+1];
+	}
+
+	delete[] arr;
+	arr = buffer;
+
+	n--;
 	return arr;
 }
